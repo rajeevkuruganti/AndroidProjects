a747c7660c0cf0d1de8f1ad3f1707b0a755d6271
Rajeev <Kuruganti>
2016-02-16 21:07:18 -0500

    Added clear, done toggle and cleaned up screen

--- OMGApp/app/src/main/java/com/example/fourcircles/omgapp/OMGActivity.java
+++ OMGApp/app/src/main/java/com/example/fourcircles/omgapp/OMGActivity.java
@@ -24,6 +24,7 @@ import java.util.ArrayList;
 public class OMGActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
     TextView t1;
     Button myFirstBtn;
+    Button myClearBtn;
     ListView mainListView;
     ArrayAdapter mArrayAdapter;
     ArrayList mToDoList = new ArrayList();
@@ -43,6 +44,8 @@ public class OMGActivity extends AppCompatActivity implements View.OnClickListen
         t1 = (TextView) findViewById(R.id.TodoText);
         myFirstBtn = (Button) findViewById(R.id.main_button);
         myFirstBtn.setOnClickListener(this);
+        myClearBtn = (Button) findViewById(R.id.main_button2);
+        myClearBtn.setOnClickListener(this);
         mainListView = (ListView) findViewById(R.id.listViewNames);
         // Create an ArrayAdapter for the ListView
         mArrayAdapter = new ArrayAdapter(this,
@@ -122,11 +125,25 @@ public class OMGActivity extends AppCompatActivity implements View.OnClickListen
 
     @Override
     public void onClick(View v) {
-        //t1.setText("Button preseeed.. l");
-        myFirstBtn.setText("Add");
-        mToDoList.add(t1.getText().toString());
-        //System.out.println("mToDoList.size() = "+mToDoList.size());
-        mArrayAdapter.notifyDataSetChanged();
+        System.out.println("what is ur ID -- " + v.getId());
+
+
+       switch (v.getId()) {
+            case R.id.main_button:
+                mToDoList.add(t1.getText().toString());
+                t1.setText("New task");
+                mArrayAdapter.notifyDataSetChanged();
+                break;
+
+            case R.id.main_button2:
+                mToDoList.clear();
+                mArrayAdapter.notifyDataSetChanged();
+                break;
+
+            default:
+                break;
+        }
+
 //        myFirstBtn.setActivated(false);
     }
     // Attaches a long click listener to the listview
@@ -137,9 +154,19 @@ public class OMGActivity extends AppCompatActivity implements View.OnClickListen
 					public boolean onItemLongClick(AdapterView<?> adapter,
 												   View item, int pos, long id) {
 						// Remove the item within array at position
-						//        mToDoList.remove(pos);
+                        int position = pos;
+                        String x = (String) mToDoList.get(pos);
+                        mToDoList.remove(position);
+                        // toggle if done or not done.
+                        if (x.indexOf(" -- Done") > 0 ){
+                             x= x.replaceFirst(" -- Done", "");
+                        } else {
+                            x= x+" -- Done";
+                        }
 
-						// Refresh the adapter
+                        mToDoList.add(position,x);
+						        //mToDoList.remove(pos);
+                        // Refresh the adapter
 						mArrayAdapter.notifyDataSetChanged();
 						// Return true consumes the long click event (marks it handled)
 						return true;
@@ -150,10 +177,8 @@ public class OMGActivity extends AppCompatActivity implements View.OnClickListen
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         //System.out.println (" the name"+parent.getItemAtPosition(position).toString());
-		//w tItemView = (TextView) v.findViewById(R.id.textView);
-		t1.setPaintFlags(t1.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
-		//mainListView.findViewById(id);
-		String x = mToDoList.get(id);
-        t1.setText(parent.getItemAtPosition(position).toString());
+
+        //t1.setText(parent.getItemAtPosition(position).toString());
     }
+
 }
