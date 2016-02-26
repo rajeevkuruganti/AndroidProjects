9c5bed627bd60bcb7d65f467aed78be2bc3ed7d7
Rajeev <Kuruganti>
2016-02-26 00:11:08 -0500

    changed package for deployment

--- OMGApp/app/src/androidTest/java/com/fourcircles/todolist/ApplicationTest.java
+++ OMGApp/app/src/androidTest/java/com/fourcircles/todolist/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.example.fourcircles.omgapp;
+package com.fourcircles.todolist;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- OMGApp/app/src/main/java/com/fourcircles/todolist/OMGActivity.java
+++ OMGApp/app/src/main/java/com/fourcircles/todolist/OMGActivity.java
@@ -1,4 +1,4 @@
-package com.example.fourcircles.omgapp;
+package com.fourcircles.todolist;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
